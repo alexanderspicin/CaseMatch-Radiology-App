21dc977feb9d9c867a5fdcb2909e3d861af05fcb
alexanderspicin <spicin.2011@mail.ru>
2025-12-17 14:27:31 +0100

    Rename files

--- src/api/users.py
+++ src/api/users.py
@@ -3,8 +3,8 @@ from sqlalchemy.orm import Session
 
 from src.db.database import get_db
 from src.models.user import User
-from src.schemas.user import UserCreateSchema, UserLoginSchema
-from src.services.auth import create_access_token, JWTBearer
+from src.schemas.user_schemas import UserCreateSchema, UserLoginSchema
+from src.services.auth_service import create_access_token, JWTBearer
 from src.services.user_service import get_password_hash, verify_password
 
 router = APIRouter(prefix="/user", tags=["user"])
